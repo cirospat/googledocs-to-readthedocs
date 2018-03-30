80a49b853267a757fb5cfd92121943dd92e36758
ciro <cirospat@gmail.com>
2018-03-30 13:53:51 +0200

    Create conf.py

---
+++ conf.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import unicode_literals
+import sys, os
+
+on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
+
+sys.path.append(os.path.abspath(os.pardir))
+
+__version__ = '1.0'
+
+# -- General configuration -----------------------------------------------------
+
+source_suffix = '.rst'
+master_doc = 'index'
+project = 'Da Google_Docs a Read_the_Docs'
+copyright = '= licenza CC BY cirospat'
+
+# The name of the Pygments (syntax highlighting) style to use.
+pygments_style = 'sphinx'
+
+extlinks = {}
+
+# -- Options for HTML output ---------------------------------------------------
+
+html_theme = 'default'
+
+html_static_path = ['static']
+
+def setup(app):
+    # overrides for wide tables in RTD theme
+    app.add_stylesheet('theme_overrides.css') # path relative to static
+
+"""
+  You might want to uncomment the “latex_documents = []” if you use CKJ characters in your document.
+  Because the pdflatex raises exception when generate Latex documents with CKJ characters.
+"""
+#latex_documents = []
+latex_logo = "static/ods.png"
+html_logo = "static/ods.png"
